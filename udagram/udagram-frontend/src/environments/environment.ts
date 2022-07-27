75783abe459ae065aa480bf2119d4f84981ca4cc
yousef hamaki <yousefhamaki2@gmail.com>
2022-07-27 05:54:47 +0200

    first upload

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "http://udagram-api-dev.eba-xqzuamxf.us-east-1.elasticbeanstalk.com/",
 };
 
 /*
